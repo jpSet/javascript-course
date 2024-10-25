fa7ecfb6c7b8e6761725eddb577a144ca0c7fbd0
jpalm <jpvalmeida@gmail.com>
2024-10-25 12:31:17 +0100

    upload lessons to git

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+let country = 'Portugal';
+let continent = 'Europe';
+let language;
+let population = 11;
+
+language='Portuguese';
+
+let isIsland = false;
+console.log(`${country} is a island -> ${isIsland} :- ` + typeof (isIsland));
+console.log(`Population -> ${population} million :- ` + typeof (population));
+console.log(`Country -> ${country} :- ` + typeof (country));
+console.log(`Language -> ${language} :- ` + typeof (language));
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+let firstName = 'Jonas'
+console.log(firstName);
+
+const COUNTRY = 'Portugal';
+const CONTINENT = 'Europe';
+let population = 10;
+
+population++;
+
+console.log(COUNTRY);
+console.log(CONTINENT);
+console.log(population);
+
+let message = `${COUNTRY} is country from ${CONTINENT} with a population of ${population} million people.`; 
