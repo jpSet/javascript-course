18893d82e30de7d93de3621d11626352b9cfa026
jpalm <jpvalmeida@gmail.com>
2024-10-26 23:12:55 +0100

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -4,7 +4,26 @@
 let hasDriversLicense = false;
 
 const passTest = true;
-if(passTest) hasDriversLicense=true;
-if(hasDriversLicense)console.log('I can drive :D');
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
 
+function logger() {
+    console.log('My Name is Jonas');
+}
 
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrageJuice = fruitProcessor(2, 4);
+console.log(appleOrageJuice);
